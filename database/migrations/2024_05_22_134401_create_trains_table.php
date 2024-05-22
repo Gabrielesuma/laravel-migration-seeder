77270e8f92c1017b57aa427eb8eb22a1d326c787
Gabrielesuma <gabrielesuma139@gmail.com>
2024-05-22 17:14:48 +0200

    migration

---
+++ database/migrations/2024_05_22_134401_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 255);
+            $table->string('departure_station',255);
+            $table->string('arrival_station',255);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('code', 255);
+            $table->integer('carriage', 2)->unsigned();
+            $table->boolean('in_time');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
